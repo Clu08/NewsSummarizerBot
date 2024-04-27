898861093135a45d3d538aa39af20d8fa8fb583a
anton aksenov <anton.axenov.anton@gmail.com>
2024-04-27 17:37:29 +0300

    Small fix of LanguageModelService interface

--- src/main/kotlin/service/languageModel/LanguageModelService.kt
+++ src/main/kotlin/service/languageModel/LanguageModelService.kt
@@ -2,9 +2,10 @@ package prod.prog.service.languageModel
 
 import prod.prog.dataTypes.Company
 import prod.prog.dataTypes.NewsPiece
+import prod.prog.dataTypes.NewsSummary
 
 interface LanguageModelService {
 //    todo functions to work with language model
 
-    fun summarizeNewsPieceByCompany(company: Company, newsPiece: NewsPiece)
+    fun summarizeNewsPieceByCompany(company: Company, newsPiece: NewsPiece) : NewsSummary
 }
