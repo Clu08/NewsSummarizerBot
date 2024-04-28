ab267de6242c8526b903db6a6ab0459d40e6c027
anton aksenov <anton.axenov.anton@gmail.com>
2024-04-28 14:36:46 +0300

    curTime moved to LoggerService

--- src/main/kotlin/service/logger/LoggerService.kt
+++ src/main/kotlin/service/logger/LoggerService.kt
@@ -1,9 +1,16 @@
 package prod.prog.service.logger
 
 import prod.prog.actionProperties.print.PrintFatal
+import java.text.SimpleDateFormat
+import java.util.*
 
 interface LoggerService {
     fun log(logLevel: PrintFatal, message: String)
+
+    fun curTime(): String {
+        val dateFormat = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        return dateFormat.format(Date())
+    }
 
     fun andThen(other: LoggerService): LoggerService =
         object : LoggerService {
