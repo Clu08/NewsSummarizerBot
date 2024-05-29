a8c76158df99fd40e95828f01636b56f68f39d49
daria-panukhina <pandapdm1999@gmail.com>
2024-05-30 00:16:09 +0300

    add bot

--- src/main/kotlin/service/manager/TelegramBot.kt
+++ src/main/kotlin/service/manager/TelegramBot.kt
@@ -2,13 +2,17 @@ package prod.prog.service.manager
 
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
-import com.github.kotlintelegrambot.dispatcher.handlers.Handler
 import com.github.kotlintelegrambot.dispatcher.handlers.TextHandlerEnvironment
+import com.github.kotlintelegrambot.dispatcher.callbackQuery
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
 import com.github.kotlintelegrambot.entities.Message
 import io.github.cdimascio.dotenv.dotenv
 import prod.prog.actionProperties.contextFactory.print.PrintInfo
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.entities.BotCommand
+import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
+import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
 import prod.prog.request.resultHandler.IgnoreErrorHandler
 import prod.prog.request.resultHandler.IgnoreHandler
 import prod.prog.request.source.ConstantSource
@@ -20,13 +24,56 @@ class TelegramBot(supervisor: Supervisor, private val logger: LoggerService) : R
     override fun name() = "TelegramBot"
     private val telegramApiToken = dotenv()["TELEGRAM_API_TOKEN"]
     private val telegramApiAddress = dotenv()["TELEGRAM_API_ADDRESS"]
+    private val config = ConfigLoader.loadConfig()
     private val telegramBot = bot {
         token = telegramApiToken
         dispatch {
-            text {
-                handleTextRequest()
+            command("start") {
+                bot.sendMessage(
+                    ChatId.fromId(message.chat.id),
+                    text = "Welcome to the bot! Use /list to see company buttons."
+                )
+            }
+            command("list") {
+                val chatId = update.message?.chat?.id ?: return@command
+                val buttons = config.buttons.map { InlineKeyboardButton.CallbackData(it.displayName, it.displayName) }
+                val inlineKeyboardMarkup = InlineKeyboardMarkup.create(buttons.chunked(1))
+                bot.sendMessage(
+                    chatId = ChatId.fromId(chatId),
+                    text = "Choose a company:",
+                    replyMarkup = inlineKeyboardMarkup
+                )
+            }
+
+            callbackQuery {
+                val chatId = update.callbackQuery?.message?.chat?.id ?: return@callbackQuery
+                val name = update.callbackQuery!!.data
+                makeRequest(
+                    RssSource(Company(name), NewsRss()),
+                    IdTransformer(),
+                    IgnoreHandler(),
+                    IgnoreErrorHandler()
+                ).thenApply { res ->
+                    sendLargeMessage(chatId = ChatId.fromId(chatId), res)
+                    val intro = if (res.isEmpty()) "No news for search" else "That is your result for search"
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(chatId),
+                        text = "$intro : \"$name\". \nSend /list to chose new company."
+                    )
+                }
             }
         }
+    }
+    init {
+        setBotCommands()
+    }
+    private fun setBotCommands() {
+        telegramBot.setMyCommands(
+            listOf(
+                BotCommand(command = "start", description = "Start the bot"),
+                BotCommand(command = "list", description = "List available companies")
+            )
+        )
     }
 
     override fun start() {
