c85e611ddb2ed104f9693cb51d6bc4d6b6b7bf3b
daria-panukhina <pandapdm1999@gmail.com>
2024-06-06 23:29:45 +0300

    add telegram bot

--- src/main/kotlin/dataTypes/rss/AvailableRssSources.kt
+++ src/main/kotlin/dataTypes/rss/AvailableRssSources.kt
@@ -10,8 +10,13 @@ import prod.prog.utils.getCharacterDataFromElement
  */
 enum class AvailableRssSources(val rssNewsLink: RssNewsLink) {
     LENTA(LentaRssNewsLink()),
-    RBK(RbkRssNewsLink())
+    RBK(RbkRssNewsLink()),
+    KOMMERSANT(KommersantRssNewsLink()),
+    RAMBLER(RamblerRssNewsLink()),
+    BIGGERPOCKETS(BiggerPocketsRssNewsLink()),
+    NYTIMES(NYtimesRssNewsLink()),
 }
+
 
 private class LentaRssNewsLink : RssNewsLink("https://lenta.ru/rss/news") {
     override fun newsParser(newsData: Element): NewsPiece {
@@ -24,6 +29,66 @@ private class LentaRssNewsLink : RssNewsLink("https://lenta.ru/rss/news") {
 }
 
 private class RbkRssNewsLink : RssNewsLink("https://rssexport.rbc.ru/rbcnews/news/30/full.rss") {
+    override fun newsParser(newsData: Element): NewsPiece {
+        return NewsPiece(
+            link = newsData.getElementsByTagName("link").item(0)?.textContent ?: "",
+            title = newsData.getElementsByTagName("title").item(0)?.textContent ?: "",
+            text = newsData.getElementsByTagName("rbc_news:full-text").item(0)?.textContent ?: "",
+        )
+    }
+}
+
+private class VedomostiRssNewsLink : RssNewsLink("https://www.vedomosti.ru/rss/news.xml") {
+    override fun newsParser(newsData: Element): NewsPiece {
+        return NewsPiece(
+            link = newsData.getElementsByTagName("link").item(0)?.textContent ?: "",
+            title = newsData.getElementsByTagName("title").item(0)?.textContent ?: "",
+            text = "",
+        )
+    }
+}
+
+private class RamblerRssNewsLink : RssNewsLink("https://finance.rambler.ru/rss/business/") {
+    override fun newsParser(newsData: Element): NewsPiece {
+        return NewsPiece(
+            link = newsData.getElementsByTagName("link").item(0)?.textContent ?: "",
+            title = newsData.getElementsByTagName("title").item(0)?.textContent ?: "",
+            text = getCharacterDataFromElement(newsData.getElementsByTagName("description").item(0) as? Element),
+        )
+    }
+}
+
+private class KommersantRssNewsLink : RssNewsLink("https://www.kommersant.ru/RSS/news.xml") {
+    override fun newsParser(newsData: Element): NewsPiece {
+        return NewsPiece(
+            link = newsData.getElementsByTagName("link").item(0)?.textContent ?: "",
+            title = newsData.getElementsByTagName("title").item(0)?.textContent ?: "",
+            text = getCharacterDataFromElement(newsData.getElementsByTagName("description").item(0) as? Element),
+        )
+    }
+}
+
+private class BBCRssNewsLink : RssNewsLink("https://feeds.bbci.co.uk/news/world/rss.xml") {
+    override fun newsParser(newsData: Element): NewsPiece {
+        return NewsPiece(
+            link = newsData.getElementsByTagName("link").item(0)?.textContent ?: "",
+            title = newsData.getElementsByTagName("title").item(0)?.textContent ?: "",
+            text = getCharacterDataFromElement(newsData.getElementsByTagName("description").item(0) as? Element),
+        )
+    }
+}
+
+private class BiggerPocketsRssNewsLink : RssNewsLink("https://www.biggerpockets.com/blog/feed") {
+    override fun newsParser(newsData: Element): NewsPiece {
+        return NewsPiece(
+            link = newsData.getElementsByTagName("link").item(0)?.textContent ?: "",
+            title = newsData.getElementsByTagName("title").item(0)?.textContent ?: "",
+            text = getCharacterDataFromElement(newsData.getElementsByTagName("description").item(0) as? Element),
+        )
+    }
+}
+
+private class NYtimesRssNewsLink : RssNewsLink("https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/world/rss.xml") {
     override fun newsParser(newsData: Element): NewsPiece {
         return NewsPiece(
             link = newsData.getElementsByTagName("link").item(0)?.textContent ?: "",
--- src/main/kotlin/service/manager/TelegramBot.kt
+++ src/main/kotlin/service/manager/TelegramBot.kt
@@ -2,25 +2,24 @@ package prod.prog.service.manager
 
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
-import com.github.kotlintelegrambot.dispatcher.handlers.TextHandlerEnvironment
 import com.github.kotlintelegrambot.dispatcher.callbackQuery
-import com.github.kotlintelegrambot.dispatcher.text
-import com.github.kotlintelegrambot.entities.ChatId
-import com.github.kotlintelegrambot.entities.Message
 import io.github.cdimascio.dotenv.dotenv
 import prod.prog.actionProperties.contextFactory.print.PrintInfo
 import com.github.kotlintelegrambot.dispatcher.command
-import com.github.kotlintelegrambot.entities.BotCommand
-import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
+import com.github.kotlintelegrambot.entities.*
 import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
+import prod.prog.dataTypes.Company
+import prod.prog.dataTypes.rss.AvailableRssSources
+import prod.prog.dataTypes.rss.NewsPiecesByCompanyRssSource
 import prod.prog.request.resultHandler.IgnoreErrorHandler
 import prod.prog.request.resultHandler.IgnoreHandler
-import prod.prog.request.source.ConstantSource
 import prod.prog.request.transformer.IdTransformer
 import prod.prog.service.logger.LoggerService
+import prod.prog.service.rss.RssService
 import prod.prog.service.supervisor.Supervisor
+import java.io.Serializable
 
-class TelegramBot(supervisor: Supervisor, private val logger: LoggerService) : RequestManager(supervisor) {
+class TelegramBot(supervisor: Supervisor, private val logger: LoggerService, private val rssService : RssService) : RequestManager(supervisor) {
     override fun name() = "TelegramBot"
     private val telegramApiToken = dotenv()["TELEGRAM_API_TOKEN"]
     private val telegramApiAddress = dotenv()["TELEGRAM_API_ADDRESS"]
@@ -29,12 +28,16 @@ class TelegramBot(supervisor: Supervisor, private val logger: LoggerService) : R
         token = telegramApiToken
         dispatch {
             command("start") {
+                logMessage(update.message!!)
+                logger.log(PrintInfo, "Received command: /start")
                 bot.sendMessage(
                     ChatId.fromId(message.chat.id),
                     text = "Welcome to the bot! Use /list to see company buttons."
                 )
             }
             command("list") {
+                logMessage(update.message!!)
+                logger.log(PrintInfo, "Received command: /list")
                 val chatId = update.message?.chat?.id ?: return@command
                 val buttons = config.buttons.map { InlineKeyboardButton.CallbackData(it.displayName, it.displayName) }
                 val inlineKeyboardMarkup = InlineKeyboardMarkup.create(buttons.chunked(1))
@@ -48,14 +51,17 @@ class TelegramBot(supervisor: Supervisor, private val logger: LoggerService) : R
             callbackQuery {
                 val chatId = update.callbackQuery?.message?.chat?.id ?: return@callbackQuery
                 val name = update.callbackQuery!!.data
+                logMessage(update.callbackQuery!!.message!!)
+                logger.log(PrintInfo, "Callback query with data: $name")
                 makeRequest(
-                    RssSource(Company(name), NewsRss()),
+                    NewsPiecesByCompanyRssSource(company = Company(name), rssService = rssService, rssNewsLinks = AvailableRssSources.entries.map { s -> s.rssNewsLink }),
                     IdTransformer(),
                     IgnoreHandler(),
                     IgnoreErrorHandler()
                 ).thenApply { res ->
                     sendLargeMessage(chatId = ChatId.fromId(chatId), res)
                     val intro = if (res.isEmpty()) "No news for search" else "That is your result for search"
+                    logger.log(PrintInfo, if (res.isEmpty()) "No info for $name found" else "Info for $name not empty")
                     bot.sendMessage(
                         chatId = ChatId.fromId(chatId),
                         text = "$intro : \"$name\". \nSend /list to chose new company."
@@ -86,19 +92,27 @@ class TelegramBot(supervisor: Supervisor, private val logger: LoggerService) : R
         logger.log(PrintInfo, "polling stopped for $telegramApiAddress")
     }
 
-    private fun TextHandlerEnvironment.handleTextRequest() {
-        val chatId = ChatId.fromId(message.chat.id)
-
-        logMessage(message)
-
-        makeRequest(
-            ConstantSource("hi, $text!"),
-            IdTransformer(),
-            IgnoreHandler(),
-            IgnoreErrorHandler()
-        ).thenApply { res ->
-            bot.sendMessage(chatId, text = res)
+    fun sendLargeMessage(chatId: ChatId, textList: List<Serializable>, beg: Int = 0) {
+        if (beg >= textList.size) {
+            return
         }
+        val maxLen = 4095
+        var text = StringBuilder()
+
+        for (i in beg until textList.size) {
+            val newLine = textList[i].toString()
+            if (text.length + newLine.length < maxLen) {
+                text.append(newLine)
+            } else {
+                telegramBot.sendMessage(chatId = chatId, text = text.toString(), parseMode = ParseMode.MARKDOWN_V2)
+                logger.log(PrintInfo, "Bot sent message fragment $i")
+                sendLargeMessage(chatId, textList, i)
+                return
+            }
+        }
+        telegramBot.sendMessage(chatId = chatId, text = text.toString(), parseMode = ParseMode.MARKDOWN_V2)
+        logger.log(PrintInfo, "Bot sent full message")
+
     }
 
     private fun logMessage(message: Message) {
