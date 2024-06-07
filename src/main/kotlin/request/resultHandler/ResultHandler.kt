45db3652eb0524c51031db47b2d1f621fd667e1e
anton aksenov <anton.axenov.anton@gmail.com>
2024-06-07 16:03:37 +0300

    fix for stable compile

--- src/main/kotlin/request/resultHandler/ResultHandler.kt
+++ src/main/kotlin/request/resultHandler/ResultHandler.kt
@@ -2,4 +2,4 @@ package prod.prog.request.resultHandler
 
 import prod.prog.request.transformer.Transformer
 
-abstract class ResultHandler<T> : Transformer<T, Unit>()
+typealias ResultHandler<T> = Transformer<T, Unit>
